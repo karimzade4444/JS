f215629ff9a7386e759e5a4e21fe53c8948b8e0e
Karimzade_8222 <karimzademustafa@gmail.com>
2026-02-17 22:29:26 +0500

    2nd example

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let fullName = (n1,n2)=> (`${n1} ${n2}`)
-console.log(fullName("Ali","Bedak"))
+// let fullName = (n1,n2)=> (`${n1} ${n2}`)
+// console.log(fullName("Ali","Bedak"))
 
+let cAge = (c1,c2)=>(c2-c1)
+console.log(cAge(2000,2025))
 
